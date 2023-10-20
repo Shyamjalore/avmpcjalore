b35973121cbc270fee10121eb8fac06080e14d98
manish kumar <95606167+Shyamjalore@users.noreply.github.com>
2023-10-21 00:33:53 +0530

    Update urls.py

--- avm/urls.py
+++ avm/urls.py
@@ -23,8 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('avmjalore.urls'))
-]
-if settings.DEBUG:
-    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns +=staticfiles_urlpatterns()
