b7b7562be9e21de9f4c21d6fe92a9fbf344dabab
Jatin Sharma <jatin2907sharma@gmail.com>
2023-10-22 01:06:44 +0530

    error wrapper

--- avmjalore/models.py
+++ avmjalore/models.py
@@ -8,27 +8,27 @@ class Avmform(models.Model):
     fathername = models.CharField(max_length=120)
     batch = models.IntegerField()
     passout = models.IntegerField()
-    mobile = models.CharField(max_length=10) 
+    mobile = models.CharField(max_length=10)
     presentaddress = models.TextField()
     permanentaddress = models.TextField()
     occupation = models.CharField(max_length=250)
     workaddress = models.TextField()
     qualification = models.CharField(max_length=50, default=None)
     DOB = models.DateField()
-    whatsapp = models.CharField(max_length=12, null=True, blank=True, default=None)
+    whatsapp = models.CharField(
+        max_length=12, null=True, blank=True, default=None)
     interest = models.TextField(null=True, blank=True, default=None)
-    achievement = models.TextField(default=None,null=True, blank=True)
+    achievement = models.TextField(default=None, null=True, blank=True)
     organization = models.TextField(null=True, blank=True, default=None)
     improvement = models.TextField(null=True, blank=True, default=None)
     suggestion = models.TextField(null=True, blank=True, default=None)
-    image_url = models.ImageField(upload_to="uploaded_image")
+    image_url = models.TextField()
     # WOB = models.DateField(null=True, blank=True)
-    
+
 # class Presentaddress(models.Model):
 #     houseorvillage = models.CharField(max_length=30 , default='')
-    
-    
-    #admin ke database me object ka name change karne ke liye
+
+    # admin ke database me object ka name change karne ke liye
+
     def __str__(self):
         return f"name: {self.name}, passout: {self.passout}"
-     
--- avmjalore/views.py
+++ avmjalore/views.py
@@ -1,77 +1,61 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import os
 from avmjalore.models import Avmform
 from django.contrib import messages
+from django.http import HttpResponse
 
 
-# Create your views here.
 def index(request):
-   
+
     return render(request, 'index.html')
 
+
 def avmform(request):
-    
+
     print('Invoked ----> avmform', request)
     if request.method == "POST":
-        name = request.POST.get('name')
-        fathername = request.POST.get('fathername')
-        batch = request.POST.get('batch')
-        passout = request.POST.get('passout')
-        presentaddress =request.POST.get('presentaddress') 
-        permanentaddress = request.POST.get('permanentaddress')
-        occupation = request.POST.get('occupation')
-        workaddress = request.POST.get('workaddress')
-        qualification =request.POST.get('qualification') 
-        DOB = request.POST.get('DOB')
-        mobile = request.POST.get('mobile')
-        whatsapp = request.POST.get('whatsapp')
-        interest = request.POST.get('interest')
-        achievement = request.POST.get('achievement')
-        organization = request.POST.get('organization')
-        improvement = request.POST.get('improvement')
-        suggestion = request.POST.get('suggestion')
-        uploaded_image = request.FILES['image']
-        # if uploaded_image:
-        #     img_path = os.path.join('static', 'media', 'uploaded_image', uploaded_image.name)
-        #     with open(img_path, 'wb') as img_file:
-        #        for chunk in uploaded_image.chunks():
-        #            img_file.write(chunk)
-            
+        try:
+            name = request.POST.get('name')
+            fathername = request.POST.get('fathername')
+            batch = request.POST.get('batch')
+            passout = request.POST.get('passout')
+            presentaddress = request.POST.get('presentaddress')
+            permanentaddress = request.POST.get('permanentaddress')
+            occupation = request.POST.get('occupation')
+            workaddress = request.POST.get('workaddress')
+            qualification = request.POST.get('qualification')
+            DOB = request.POST.get('DOB')
+            mobile = request.POST.get('mobile')
+            whatsapp = request.POST.get('whatsapp')
+            interest = request.POST.get('interest')
+            achievement = request.POST.get('achievement')
+            organization = request.POST.get('organization')
+            improvement = request.POST.get('improvement')
+            suggestion = request.POST.get('suggestion')
+            uploaded_image = request.FILES['image']
+
+            img_path = ""
+            if uploaded_image:
+                img_path = os.path.join(
+                    'media', 'uploaded_image', uploaded_image.name)
+                with open(img_path, 'wb') as img_file:
+                    for chunk in uploaded_image.chunks():
+                        img_file.write(chunk)
+
+            render_base_url = os.environ.get('RENDER_BASE_URL')
+            image_url = f'{render_base_url}/{img_path}'
+
+            print({img_path, image_url, render_base_url})
+
             # image_url = '/media/uploaded_image/' + uploaded_image.name
-            # render_base_url = os.environ.get('RENDER_BASE_URL')
-            # image_url = f'{render_base_url}/media/uploaded_image/{uploaded_image.name}'
-        
-        #  python me kisi data ko debugg aise karte h
-        print(
-            name, 
-            fathername, 
-            batch, 
-            passout,
-            presentaddress,
-            permanentaddress,
-            occupation,
-            workaddress,
-            qualification,
-            DOB,
-            # WOB, 
-            mobile,
-            whatsapp,
-            interest,
-            achievement,
-            organization,
-            improvement,
-            suggestion,
-            uploaded_image
-          
-            
-           
-        )
-        
-        avmform = Avmform(name=name, fathername=fathername, batch=batch, passout=passout, presentaddress=presentaddress, permanentaddress=permanentaddress, occupation=occupation, workaddress=workaddress, qualification=qualification, DOB=DOB, mobile=mobile, whatsapp=whatsapp, interest=interest, achievement=achievement, organization=organization, improvement=improvement, suggestion=suggestion, image_url=uploaded_image)
-        # # # avmform = Avmform(name=name, fathername=fathername)
-        avmform.save()
-        messages.success(request, "your form is successfully submitted.")
+            avmform = Avmform(name=name, fathername=fathername, batch=batch, passout=passout, presentaddress=presentaddress, permanentaddress=permanentaddress, occupation=occupation, workaddress=workaddress,
+                              qualification=qualification, DOB=DOB, mobile=mobile, whatsapp=whatsapp, interest=interest, achievement=achievement, organization=organization, improvement=improvement, suggestion=suggestion, image_url=image_url)
+            avmform.save()
+            messages.success(request, "your form is successfully submitted.")
+        except Exception as e:
+            error_message = f"An error occurred: {str(e)}"
+
+            return HttpResponse(error_message, status=500)
         return redirect("/")
-        
-    
-    return render(request,'avmform.html')
+
+    return render(request, 'avmform.html')
